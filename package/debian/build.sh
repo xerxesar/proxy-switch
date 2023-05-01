15d5d1723c95faf1ac8d5b00f684589f1b7f05a0
xerxesar <khashayar.aryanmehr@gmail.com>
2023-05-02 01:29:53 +0330

    Add debian packaging
    Package the script as .deb, add 'prxy' link on postinst.
    Upload revision 3 build of version 0.1

---
+++ package/debian/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+# create debian package
+DEBIAN_VERSION="0.1-3"
+PKG_PATH="./proxy-switch_${DEBIAN_VERSION}_amd64"
+mkdir -p $PKG_PATH/DEBIAN
+cp ./control ./postinst $PKG_PATH/DEBIAN/
+mkdir -p $PKG_PATH/usr/local/bin
+cp ../../proxy-switch $PKG_PATH/usr/local/bin/
+chmod +x $PKG_PATH/usr/local/bin/proxy-switch
+dpkg-deb --build --root-owner-group proxy-switch_${DEBIAN_VERSION}_amd64
